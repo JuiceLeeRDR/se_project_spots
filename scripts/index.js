62af3f34e52c722acb53f2a8fa3c1b0d7409f57c
Raiquan Harris <rayharris0895@gmail.com>
2025-01-18 18:36:20 -0500

    JS update to Spots project

---
+++ scripts/index.js
@@ -0,0 +1,26 @@
+const initialCards = [
+  {
+    name: "Walking through a Japanese side street ",
+    link: "https://unsplash.com/photos/man-walking-between-stores-during-night-time-LA57RXF1mSI",
+  },
+  {
+    name: "Outdoor cafe near stream in Italy",
+    link: "https://unsplash.com/photos/man-riding-on-boat-beside-restaurant-Uu5aXBI1oLk",
+  },
+  {
+    name: "Mount Oso, Japan",
+    link: "https://unsplash.com/photos/photo-of-mountain-peak-ryUr3SC6Lpo",
+  },
+  {
+    name: "Pyramid in Egypt",
+    link: "https://unsplash.com/photos/brown-pyramid-during-daytime-FXXuJ9S-KkQ",
+  },
+  {
+    name: "View of ocean from villa steps in Greece",
+    link: "https://unsplash.com/photos/santorini-greece-XGKaRnWjv1c",
+  },
+  {
+    name: "Downtown Tokyo",
+    link: "https://unsplash.com/photos/people-walking-on-road-near-well-lit-buildings-layMbSJ3YOE",
+  },
+];

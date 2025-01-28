95d043dc8a296b43f8481ec6fce293d0dd472274
Raiquan Harris <rayharris0895@gmail.com>
2025-01-28 16:05:15 -0500

    final stage, sprint 4

--- scripts/index.js
+++ scripts/index.js
@@ -1,38 +1,81 @@
 const initialCards = [
   {
     name: "Walking through a Japanese side street ",
-    link: "https://unsplash.com/photos/man-walking-between-stores-during-night-time-LA57RXF1mSI",
+    link: "https://images.unsplash.com/photo-1508504509543-5ca56440e013?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
     name: "Outdoor cafe near stream in Italy",
-    link: "https://unsplash.com/photos/man-riding-on-boat-beside-restaurant-Uu5aXBI1oLk",
+    link: "https://images.unsplash.com/photo-1553342385-111fd6bc6ab3?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
     name: "Mount Oso, Japan",
-    link: "https://unsplash.com/photos/photo-of-mountain-peak-ryUr3SC6Lpo",
+    link: "https://images.unsplash.com/photo-1520312501384-dbdb83a1cb11?q=80&w=1975&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
     name: "Pyramid in Egypt",
-    link: "https://unsplash.com/photos/brown-pyramid-during-daytime-FXXuJ9S-KkQ",
+    link: "https://images.unsplash.com/photo-1562679299-d21b8e13ac09?q=80&w=1972&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
     name: "View of ocean from villa steps in Greece",
-    link: "https://unsplash.com/photos/santorini-greece-XGKaRnWjv1c",
+    link: "https://images.unsplash.com/photo-1533105079780-92b9be482077?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
     name: "Downtown Tokyo",
-    link: "https://unsplash.com/photos/people-walking-on-road-near-well-lit-buildings-layMbSJ3YOE",
+    link: "https://images.unsplash.com/photo-1540959733332-eab4deabeeaf?q=80&w=2094&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
 ];
 
 const profileEditButton = document.querySelector(".profile__edit-button");
 const profileEditModal = document.querySelector("#edit-modal");
 const profileEditExitButton = document.querySelector(".modal__close-button");
+const proileEditModalForm = profileEditModal.querySelector(".modal__form");
+const profileName = document.querySelector(".profile__name");
+const profileDescription = document.querySelector(".profile__description");
+const modalInputName = document.querySelector("#profile-name-input");
+const modalInputDescription = document.querySelector(
+  "#profile-description-input"
+);
+const cardTemplate = document.querySelector("#card-template");
+const cardsList = document.querySelector(".cards__list");
 
-profileEditButton.addEventListener("click", function () {
+function getCardElement(data) {
+  const cardElement = cardTemplate.content
+    .querySelector(".card")
+    .cloneNode(true);
+
+  const cardNameElement = cardElement.querySelector(".card__text");
+  const cardImage = cardElement.querySelector(".card__image");
+
+  cardNameElement.textContent = data.name;
+  console.log(data.link);
+  cardImage.src = data.link;
+
+  return cardElement;
+}
+
+function editProfile() {
+  modalInputName.value = profileName.textContent;
+  modalInputDescription.value = profileDescription.textContent;
   profileEditModal.classList.add("modal__opened");
-});
+}
 
-profileEditExitButton.addEventListener("click", function () {
+function closeProfile() {
   profileEditModal.classList.remove("modal__opened");
-});
+}
+
+function handleEditProfileFormSubmit(evt) {
+  evt.preventDefault();
+  profileName.textContent = modalInputName.value;
+  profileDescription.textContent = modalInputDescription.value;
+  profileEditModal.classList.remove("modal__opened");
+}
+
+profileEditButton.addEventListener("click", editProfile);
+
+profileEditExitButton.addEventListener("click", closeProfile);
+proileEditModalForm.addEventListener("submit", handleEditProfileFormSubmit);
+
+for (let i = 0; i < initialCards.length; i++) {
+  const cardElement = getCardElement(initialCards[i]);
+  cardsList.prepend(cardElement);
+}
